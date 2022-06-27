8552725a31465224eeb0f5174b74cdd7b27be7d6
Dávid Bangó <ynvaser@hotmail.com>
2022-06-28 00:13:56 +0200

    Try to trigger CICD

--- src/main/java/systems/bdev/deckscraper/DeckScraperApplication.java
+++ src/main/java/systems/bdev/deckscraper/DeckScraperApplication.java
@@ -11,6 +11,7 @@ import systems.bdev.deckscraper.util.Utils;
 @Slf4j
 public class DeckScraperApplication {
     public static void main(String[] args) {
+        log.info("Application started, please be patient...");
         ConfigurableApplicationContext applicationContext = SpringApplication.run(DeckScraperApplication.class, args);
         applicationContext.getBean(DeckScraperService.class).run(args);
         log.info("Application finished, please check the output folder for results.");
