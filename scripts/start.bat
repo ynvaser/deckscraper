b4c89b28e562b2c4eb60b017d3f108e3205b6591
Dávid Bangó <ynvaser@hotmail.com>
2022-12-25 20:14:19 +0100

    Add double quotes to path in batch file

--- scripts/start.bat
+++ scripts/start.bat
@@ -1,5 +1,5 @@
 SET mypath=%~dp0
-cd %mypath%
+cd "%mypath%"
 echo "Running batch script from %mypath%"
 @REM This is the required percentage of a given deck you need to own for it to be saved to the output folders.
 SET threshold=80
@@ -13,4 +13,4 @@ SET maxlands=60
 SET averageDeckThreshold=80
 @REM Set this to true if you'd like to scrape decks for every commander. This will significantly increase the time for the program to finish.
 SET searchUnownedCommanders=false
-java -Xmx4G -jar deckscraper.jar %mypath% %threshold% %months% %skipLookup% %maxlands% %averageDeckThreshold% %searchUnownedCommanders%
+java -Xmx4G -jar deckscraper.jar "%mypath%" %threshold% %months% %skipLookup% %maxlands% %averageDeckThreshold% %searchUnownedCommanders%
